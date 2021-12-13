43d13df3d0d5bdbcf00ae6b93c3760c6b35fa086
gobi.krishna@zohocorp.com <gobi.krishna@zohocorp.com>
2021-12-13 17:31:35 +0530

    add Record

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 ZOHO.CREATOR.init().then(function(data) {
-    console.log(data);
     config = {
         formName : "Form1",
         data : {
@@ -10,8 +9,25 @@ ZOHO.CREATOR.init().then(function(data) {
         }
     }
     //add record API
-    ZOHO.CREATOR.API.addRecord(config).then(function(response){
+    // ZOHO.CREATOR.API.addRecord(config).then(function(response){
+    //     //callback block
+    //     console.log("addRecord: ", response);
+    // });
+
+
+    config = {
+        reportName : "Form1_Report",
+        id: "88557000000237003",
+        data : {
+            "data" : {
+                Number: 101,
+                Single_Line: "Gobi Krishna S"
+            }
+        }
+    }
+    //update record API
+    ZOHO.CREATOR.API.updateRecord(config).then(function(response){
         //callback block
-        console.log("addRecord: ", response);
+        console.log("updateRecord: ", response);
     });
 });
