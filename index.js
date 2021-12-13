e2f6e4e8ef4a3c8813809738335675278894abd4
gobi.krishna@zohocorp.com <gobi.krishna@zohocorp.com>
2021-12-13 17:12:42 +0530

    add Record

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ ZOHO.CREATOR.init().then(function(data) {
     console.log(data);
     config = {
         formName : "Form1",
-        data : ""
+        data : {
+            Number: 101,
+            Single_Line: "Krishna"
+        }
     }
     //add record API
     ZOHO.CREATOR.API.addRecord(config).then(function(response){
