64064e66534a509610d40ef786afdab2a585f17a
gobi.krishna@zohocorp.com <gobi.krishna@zohocorp.com>
2021-12-14 11:58:07 +0530

    changes

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 
 ZOHO.CREATOR.init().then(function(data) {
-    config = {
-        formName : "Form1",
-        data : {
-            "data" : {
-                Number: 101,
-                Single_Line: "Krishna"
-            }
-        }
-    }
-    //add record API
-    ZOHO.CREATOR.API.addRecord(config).then(function(response){
-        console.log("addRecord: ", response);
-    });
+    // config = {
+    //     formName : "Form1",
+    //     data : {
+    //         "data" : {
+    //             Number: 101,
+    //             Single_Line: "Krishna"
+    //         }
+    //     }
+    // }
+    // //add record API
+    // ZOHO.CREATOR.API.addRecord(config).then(function(response){
+    //     console.log("addRecord: ", response);
+    // });
 
-    config = {
-        reportName : "Form1_Report",
-        id: 88557000000240011,
-        data : {
-            "data" : {
-                Number: 101,
-                Single_Line: "Gobi Krishna S"
-            }
-        }
-    }
-    // update record API
-    ZOHO.CREATOR.API.updateRecord(config).then(function(response){
-        console.log("updateRecord: ", response);
-    });
+    // config = {
+    //     reportName : "Form1_Report",
+    //     id: 88557000000240011,
+    //     data : {
+    //         "data" : {
+    //             Number: 101,
+    //             Single_Line: "Gobi Krishna S"
+    //         }
+    //     }
+    // }
+    // // update record API
+    // ZOHO.CREATOR.API.updateRecord(config).then(function(response){
+    //     console.log("updateRecord: ", response);
+    // });
 
 
-    config = { 
-        reportName : "Form1_Report",
-        page : 1,
-        pageSize : 10
-    }
-    //get all record API
-    ZOHO.CREATOR.API.getAllRecords(config).then(function(response){
-        console.log(response);
-    });
+    // config = { 
+    //     reportName : "Form1_Report",
+    //     page : 1,
+    //     pageSize : 10
+    // }
+    // //get all record API
+    // ZOHO.CREATOR.API.getAllRecords(config).then(function(response){
+    //     console.log(response);
+    // });
 
 
     var config = {
@@ -48,19 +48,22 @@ ZOHO.CREATOR.init().then(function(data) {
     //get record by Id API
     ZOHO.CREATOR.API.getRecordById(config).then(function(response){
         console.log(response);
+    }).catch(function(data){
+        console.log("error occurred");
+        console.log(data);
     });
 
-    var config = { 
-        reportName : "Form1_Report", 
-        criteria : "(Single_Line == \"Gobi Krishna s\")"
-    } 
-    //delete record API
-    ZOHO.CREATOR.API.deleteRecord(config).then(function(response){
-        console.log(response);
-    });
+    // var config = { 
+    //     reportName : "Form1_Report", 
+    //     criteria : "(Single_Line == \"Gobi Krishna s\")"
+    // } 
+    // //delete record API
+    // ZOHO.CREATOR.API.deleteRecord(config).then(function(response){
+    //     console.log(response);
+    // });
 
-    var queryParams = ZOHO.CREATOR.UTIL.getQueryParams();
-    console.log(queryParams);
+    // var queryParams = ZOHO.CREATOR.UTIL.getQueryParams();
+    // console.log(queryParams);
 });
 
 
